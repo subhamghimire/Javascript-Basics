72881e7933143c1125a59e7c0271bea4cd7114a7
Shekhar Ghimire <subham.ghimire26@gmail.com>
2019-11-29 15:23:39 +0545

    Add files via upload

---
+++ LearnJSOnClass/script.js
@@ -0,0 +1,54 @@
+//Nov 25 
+let button = document.querySelector("#click")
+// console.log(button)
+/*
+let p = document.querySelectorAll("p")
+console.log(p)
+
+//document.addEventListener("click", function(){
+//     console.log("clicked")
+// })
+
+// document.addEventListener("dblclick", function(){
+//     console.log("dblclicked")
+// })
+
+document.addEventListener("keyup", function(){
+    console.log("keyup")
+})
+
+//     button.addEventListener("click", function(e){
+//     console.log(e.target)
+// })
+*/
+/*
+    let hide  = document.querySelector(".hide")
+    button.addEventListener("click", function(e){
+        if (hide.style.display == "block"){
+            hide.style.display = "none"
+        } else {
+            hide.style.display = "block"
+        }
+    })
+
+//Nov 26
+let username = document.querySelector("#username")
+username.addEventListener("keyup",function(e){
+   // console.log(e.keyCode) //it shows which key pressed
+let  color = username.value.replace("color=","")
+   if (e.keyCode==13) // if entered then
+     { 
+       console.log(username.value)
+       if (username.value == 'resize'){
+           hide.style.height = '400px';
+       }
+       if(username.value == "color=red"){
+           console.log(color)
+           hide.style.backgroundColor = 'red'
+       }
+     } else {
+         console.log("Not entered yet")
+     }
+})
+*/
+
